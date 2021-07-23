114c3c5547547bfbffbff146687fe6483d15fec4
DesktopYellow <eriks@10.0.0.204>
2021-07-23 17:45:47 -0400

    d

---
+++ src/main/java/net/mcreator/oneandyelloshell/block/FdfdBlock.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.oneandyelloshell.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.oneandyelloshell.OneandyelloshellModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@OneandyelloshellModElements.ModElement.Tag
+public class FdfdBlock extends OneandyelloshellModElements.ModElement {
+	@ObjectHolder("oneandyelloshell:fdfd")
+	public static final Block block = null;
+	public FdfdBlock(OneandyelloshellModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("fdfd");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
